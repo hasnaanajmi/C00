6118b091d23fa522811dc6596dcdd589aaea0974
hasnaanajmi <najmidesign.official@gmail.com>
2025-07-08 22:27:23 +0100

    Add ex05

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -6,3 +6,11 @@ void ft_is_negative(int n) {
 	    write(1, "P", 1);
     }
 }
+
+int main(void) {
+    ft_is_negative(0);
+    write(1, "\n", 1);
+    ft_is_negative(-5);
+    ft_is_negative(44);
+    return 0;
+}
---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+void ft_print_comb(void) {
+    char comb[5];
+    comb[0] = '0';
+    comb[3] = ',';
+    comb[4] = ' ';
+    while(comb[0] <= '7') {
+        comb[1]= comb[0] + '1';
+        while(comb[1] <= '8') {
+            comb[2]= comb[1] + '1';
+            while(comb[2] <= '9') {
+                if(comb[0]== '7' && comb[1]== '8' && comb[2]== '9') write(1, comb, 3);
+                else write(1, comb, 5);
+                comb[2]++;
+            }
+            comb[1]++;
+        }
+        comb[0]++;
+    }
+}
+/*int main(void) {
+    ft_print_comb();
+    return 0;
+}*/
