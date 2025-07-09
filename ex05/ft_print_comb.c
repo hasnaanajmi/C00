a09bb721f08dfcbceace33dfb947f0ced6cd3967
hasnaanajmi <najmidesign.official@gmail.com>
2025-07-09 22:57:50 +0100

    Add ex05

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -5,9 +5,9 @@ void ft_print_comb(void) {
     comb[3] = ',';
     comb[4] = ' ';
     while(comb[0] <= '7') {
-        comb[1]= comb[0] + '1';
+        comb[1]= comb[0] + 1;
         while(comb[1] <= '8') {
-            comb[2]= comb[1] + '1';
+            comb[2]= comb[1] + 1;
             while(comb[2] <= '9') {
                 if(comb[0]== '7' && comb[1]== '8' && comb[2]== '9') write(1, comb, 3);
                 else write(1, comb, 5);
@@ -17,6 +17,7 @@ void ft_print_comb(void) {
         }
         comb[0]++;
     }
+    write(1, "\n", 1);
 }
 /*int main(void) {
     ft_print_comb();
